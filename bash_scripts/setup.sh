ecf01dd4f97c7dbf0dd7edc53fa616f7d9bc0351
David Rein <idavidrein@gmail.com>
2023-12-08 16:59:01 -0500

    download quality dataset during setup

--- bash_scripts/setup.sh
+++ bash_scripts/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 python -m spacy download en_core_web_sm
+curl -o data/datasets/quality/QuALITY.v1.0.1.htmlstripped.train https://raw.githubusercontent.com/nyu-mll/quality/main/data/v1.0.1/QuALITY.v1.0.1.htmlstripped.train
+curl -o data/datasets/quality/QuALITY.v1.0.1.htmlstripped.dev https://raw.githubusercontent.com/nyu-mll/quality/main/data/v1.0.1/QuALITY.v1.0.1.htmlstripped.dev
+curl -o data/datasets/quality/QuALITY.v1.0.1.htmlstripped.test https://raw.githubusercontent.com/nyu-mll/quality/main/data/v1.0.1/QuALITY.v1.0.1.htmlstripped.test
