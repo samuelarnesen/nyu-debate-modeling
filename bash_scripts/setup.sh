772cd52690163b2a92e361ed6bb14cd8c1264b72
David Rein <idavidrein@gmail.com>
2023-12-08 17:44:58 -0500

    mkdirs in setup.sh

--- bash_scripts/setup.sh
+++ bash_scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 python -m spacy download en_core_web_sm
+mkdir data/datasets
+mkdir data/datasets/quality
 curl -o data/datasets/quality/QuALITY.v1.0.1.htmlstripped.train https://raw.githubusercontent.com/nyu-mll/quality/main/data/v1.0.1/QuALITY.v1.0.1.htmlstripped.train
 curl -o data/datasets/quality/QuALITY.v1.0.1.htmlstripped.dev https://raw.githubusercontent.com/nyu-mll/quality/main/data/v1.0.1/QuALITY.v1.0.1.htmlstripped.dev
 curl -o data/datasets/quality/QuALITY.v1.0.1.htmlstripped.test https://raw.githubusercontent.com/nyu-mll/quality/main/data/v1.0.1/QuALITY.v1.0.1.htmlstripped.test
